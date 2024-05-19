97df577cb525d3ff5f1165ec5182533cda48aa2b
Thong Pham <thongpv87@gmail.com>
2024-05-19 17:51:35 +0700

    update nixvim config

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     nixvim = {
       url = "github:nix-community/nixvim";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
+      # inputs = { nixpkgs.follows = "nixpkgs"; };
     };
 
     hyprland = {
