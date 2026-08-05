7750d9e908c50e159cdcc622745c34471fb4ffbb
Thong Pham <thongpv87@gmail.com>
2026-08-05 16:23:57 +0700

    feat(pdf/mime): switch PDF viewer to Okular and configure xdg.mimeApps defaults

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -110,6 +110,15 @@ in
         "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
         "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
       ];
+      mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "application/pdf" = [ "org.kde.okular.desktop" ];
+          "text/html" = [ "google-chrome.desktop" ];
+          "x-scheme-handler/http" = [ "google-chrome.desktop" ];
+          "x-scheme-handler/https" = [ "google-chrome.desktop" ];
+        };
+      };
     };
 
     dconf.settings = {
@@ -131,7 +140,7 @@ in
       graphviz
       sshfs
       pdftk
-      sioyek
+      kdePackages.okular
       asciinema
 
       # Spell checking
